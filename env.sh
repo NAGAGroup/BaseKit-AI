531c2ed38ab654f4a21f2d5ed819be4e29ea30c5
Jack Myers <jack.myers@intel.com>
2024-11-21 01:27:08 +0000

    need to preload a library
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 export PATH="$CONDA_PREFIX/go/bin:$PATH"
 export GOBIN="$GOROOT/bin"
+export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
 
 echo "--sysroot=$CONDA_BUILD_SYSROOT --gcc-toolchain=$CONDA_PREFIX --target=$HOST $CXXFLAGS" >"$CONDA_PREFIX/bin/icpx.cfg"
 echo "--sysroot=$CONDA_BUILD_SYSROOT --gcc-toolchain=$CONDA_PREFIX --target=$HOST $CFLAGS" >"$CONDA_PREFIX/bin/icx.cfg"
