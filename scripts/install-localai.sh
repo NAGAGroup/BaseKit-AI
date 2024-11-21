6f239f6f1a5155a2a0b0b06680b4362aeff6eaa2
Jack Myers <jack.myers@intel.com>
2024-11-21 01:50:33 +0000

    switched to fp32
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- scripts/install-localai.sh
+++ scripts/install-localai.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export BUILD_TYPE=sycl_f16
+export BUILD_TYPE=sycl_f32
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@1958fcbe2ca8bd93af633f11e97d44e567e945af
 
 if [ ! -d "LocalAI" ]; then
